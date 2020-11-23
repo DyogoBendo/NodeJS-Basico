7b700a9b8beef9e3ae2bdb676389ebdf0036aa73
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-11-23 09:44:27 -0300

    Hello World!

---
+++ Calculadora/calc.js
@@ -0,0 +1,17 @@
+function somar(a, b){
+    return a + b
+}
+
+function subtrair(a, b){
+    return a - b
+}
+
+function multiplicar(a, b){
+    return a * b
+}
+
+function div (a, b){
+    return a / b
+}
+
+console.log(subtrair(2, 4))
