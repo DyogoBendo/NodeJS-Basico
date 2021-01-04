02361357f71b75df24bec1c2f9707bbcf1453bfa
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-01-04 19:09:14 -0300

    Guardando informações vinda dos formulários no bd

--- Cadastro_Postagens/index.js
+++ Cadastro_Postagens/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const handlebars = require('express-handlebars')
 const app = express()
 const bodyParser = require('body-parser')
+const Post = require('./models/Post')
 
 
 // Config
@@ -13,13 +14,24 @@ const bodyParser = require('body-parser')
         app.use(bodyParser.json())
 
 // Rotas
+
+    app.get('/', function(req, res){
+        res.render('home')
+    })
+
     app.get('/cad', function(req, res){
         res.render('formularios') // queremos renderizar o arquivo do tipo handlebars formulario, que está na pasta views
     })
 
     app.post('/add', function(req, res){ // como usamos o metodo post no formulario, devemos usar o mesmo metodo na rota
-        req.body.conteudo
-        res.send('Texto ' + req.body.conteudo)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/') // redirecionamos para uma outra rota
+        }).catch(function(erro){
+            res.send('Erro em criar o post!')
+        })
     })
 
 app.listen(8081, function(){
