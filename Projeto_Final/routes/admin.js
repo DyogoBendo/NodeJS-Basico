4136ec805a902f0f12c3736d904bf7d2a71cdbd2
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-01-05 10:54:49 -0300

    Inserindo valores de categorias

--- Projeto_Final/routes/admin.js
+++ Projeto_Final/routes/admin.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router() // criamos rotas em um arquivo separado assim
+const mongoose = require('mongoose')
+require('../models/Categoria')
+
+const Categoria = mongoose.model('categorias')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -15,6 +19,19 @@ router.get('/categorias', (req, res) =>{
 
 router.get('/categorias/add', (req, res) =>{
     res.render('admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req, res) =>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() =>{
+        console.log('Categoria salva com sucesso!')
+    }).catch(err =>{
+        console.log('Erro: ' + err)
+    })
 })
 
 module.exports = router // exportamos as rotas definidas nesse arquivo
