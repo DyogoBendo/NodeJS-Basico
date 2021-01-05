199039fdd3999c56c408cee689bd6c47c595596a
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-01-05 14:20:23 -0300

    Editando categorias

--- Projeto_Final/routes/admin.js
+++ Projeto_Final/routes/admin.js
@@ -71,4 +71,30 @@ router.post("/categorias/nova", (req, res) => {
   }
 });
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id : req.params.id}).then((categoria) =>{
+        res.render('admin/editcategoria', {categoria: categoria.toJSON()})
+    }).catch(err =>{
+        req.flash('error_msg', 'Esta categoria não existe')
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/edit', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then(categoria =>{
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada com sucesso')
+            res.redirect('/admin/categorias')
+        }).catch(err => {
+            req.flash('error_msg', 'Erro ao salvar os dados')
+        })
+    }).catch(err => {
+        req.flash('error_msg', 'Erro ao editar a categoria')
+        res.redirect('/admin/categorias')
+    })
+})
+
 module.exports = router; // exportamos as rotas definidas nesse arquivo
